004e0a747d4f5469ba0ae72628e5b2133997e7f8
leonardo-luz <leonardoluz.f02@gmail.com>
2025-01-05 16:54:04 -0300

    [feat] add basic quicknote

--- lua/floatnotes.lua
+++ lua/floatnotes.lua
@@ -1,7 +1,39 @@
+local floatwindow = require("floatwindow")
+
 local M = {}
 
-local state = {}
+local state = {
+  window_config = {
+    floating = {},
+    enter = true,
+  },
+  path = "",
+}
 
-M.setup = function() end
+M.quick_note = function()
+  local filename = "test"
+
+  state.window_config.floating = floatwindow.create_floating_window(state.window_config)
+
+  vim.cmd(string.format("edit %s/%s.md", state.path, filename))
+
+  vim.api.nvim_create_autocmd("bufLeave", {
+    buffer = state.window_config.floating.buf,
+    callback = function()
+      vim.cmd(string.format("w %s/%s.md", state.path, filename))
+    end,
+  })
+end
+
+vim.api.nvim_create_user_command("Quicknote", M.quick_note, {})
+
+---@class floatnotes.Setup
+---@field path string: String to where the notes will be saved
+
+---setup floatnotes plugin
+---@param opts floatnotes.Setup
+M.setup = function(opts)
+  state.path = opts.path
+end
 
 return M
